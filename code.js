396ea83e2bcb8c64e8f80f5284845655800fe492
Vladimir Bykov <milsinery@icloud.com>
2021-05-02 20:17:19 +0300

    fixed bugs and optimised

--- code.js
+++ code.js
@@ -1,19 +1,23 @@
-const allObjects = figma.currentPage.findAll(item => item.parent === figma.currentPage);
-for (const item of allObjects) {
-    item.setRelaunchData({ edit: '' });
-}
-const frame = figma.currentPage.selection.find(item => item.type === "FRAME");
-if (frame) {
-    const other = figma.currentPage.selection.filter(item => item.id !== frame.id);
-    if (other.length > 0) {
-        for (const item of other) {
-            if (item.parent === figma.currentPage) {
-                item.x = item.x - frame.x;
-                item.y = item.y - frame.y;
-                frame.appendChild(item);
-            }
-        }
+const setPluginLinkToSidebar = () => {
+    const allObjects = figma.currentPage.findAll((item) => item.parent === figma.currentPage);
+    for (const item of allObjects) {
+        item.setRelaunchData({ edit: '' });
     }
-}
+};
+const main = () => {
+    const frame = figma.currentPage.selection.find((item) => item.type === 'FRAME');
+    if (!frame)
+        return;
+    const other = figma.currentPage.selection.filter((item) => item.id !== frame.id);
+    if (other.length === 0)
+        return;
+    for (const item of other) {
+        item.x = item.x - frame.x;
+        item.y = item.y - frame.y;
+        frame.appendChild(item);
+    }
+};
+setPluginLinkToSidebar();
+main();
 figma.currentPage.setRelaunchData({ open: '' });
 figma.closePlugin();
--- code.ts
+++ code.ts
@@ -1,25 +1,34 @@
-const allObjects = figma.currentPage.findAll(item => item.parent === figma.currentPage);
+const setPluginLinkToSidebar = () => {
+  const allObjects = figma.currentPage.findAll(
+    (item) => item.parent === figma.currentPage
+  );
 
-for(const item of allObjects) {
-  item.setRelaunchData({ edit: '' });
-}
-
-const frame: FrameNode = figma.currentPage.selection.find(item => item.type === "FRAME") as FrameNode;
-
-if(frame) {
-  const other: Array<SceneNode> = figma.currentPage.selection.filter(item => item.id !== frame.id);
-
-  if(other.length > 0) {
-    for(const item of other) {
-      if(item.parent === figma.currentPage) {
-        item.x = item.x - frame.x;
-        item.y = item.y - frame.y;
-        frame.appendChild(item);
-      }
-
-    }
+  for (const item of allObjects) {
+    item.setRelaunchData({ edit: '' });
   }
-}
+};
 
+const main = () => {
+  const frame: FrameNode = figma.currentPage.selection.find(
+    (item) => item.type === 'FRAME'
+  ) as FrameNode;
+
+  if (!frame) return;
+
+  const other: Array<SceneNode> = figma.currentPage.selection.filter(
+    (item) => item.id !== frame.id
+  );
+
+  if (other.length === 0) return;
+
+  for (const item of other) {
+    item.x = item.x - frame.x;
+    item.y = item.y - frame.y;
+    frame.appendChild(item);
+  }
+};
+
+setPluginLinkToSidebar();
+main();
 figma.currentPage.setRelaunchData({ open: '' });
 figma.closePlugin();
